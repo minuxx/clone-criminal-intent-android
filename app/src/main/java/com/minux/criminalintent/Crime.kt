73faaae281c148abe1b1a620099215122c9282b0
minuxx <minux8905@gmail.com>
2023-08-03 13:18:49 +0900

    add: Crime 데이터 클래스 추가

---
+++ app/src/main/java/com/minux/criminalintent/Crime.kt
@@ -0,0 +1,11 @@
+package com.minux.criminalintent
+
+import java.util.Date
+import java.util.UUID
+
+data class Crime(
+    val id: UUID = UUID.randomUUID(),
+    var title: String = "",
+    var date: Date = Date(),
+    var isSolved: Boolean = false
+)
