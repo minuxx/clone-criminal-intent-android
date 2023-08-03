828f2273574d711b6010d62f4cc2b5637ca60163
minuxx <minux8905@gmail.com>
2023-08-03 15:01:40 +0900

    add: CrimeFragment 뷰, 리스너 연결

---
+++ app/src/main/java/com/minux/criminalintent/CrimeFragment.kt
@@ -0,0 +1,68 @@
+package com.minux.criminalintent
+
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
+import androidx.fragment.app.Fragment
+
+class CrimeFragment : Fragment() {
+    private lateinit var crime: Crime
+    private lateinit var titleField: EditText
+    private lateinit var dateButton: Button
+    private lateinit var solvedCheckBox: CheckBox
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        crime = Crime()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_crime, container, false)
+
+        titleField = view.findViewById(R.id.crime_title) as EditText
+        dateButton = view.findViewById(R.id.crime_date) as Button
+        solvedCheckBox = view.findViewById(R.id.crime_solved) as CheckBox
+
+        dateButton.apply {
+            text = crime.date.toString()
+            isEnabled = false
+        }
+
+        return view
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        // EditText 의 TextWatcher나 CheckBox 의 OnCheckChangedListener 등 데이터 입력에 반응하는 리스너들은
+        // 사용자와 상호 작용할 때뿐만 아니라 장치 회전 등으로 인해 뷰 상태가 복원되면서 데이터가 설정될 때도 작동되기 때문에
+        // onStart 에서 설정해줘야 한다.
+        val titleWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                crime.title = s.toString()
+            }
+
+            override fun afterTextChanged(s: Editable?) {}
+        }
+
+        titleField.addTextChangedListener(titleWatcher)
+
+        solvedCheckBox.apply {
+            setOnCheckedChangeListener { _, isChecked ->
+                crime.isSolved = isChecked
+            }
+        }
+    }
+}
