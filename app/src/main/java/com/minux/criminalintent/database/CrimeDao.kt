83207949be24bf1132b0dace090ce7e74f8624b8
minuxx <minux8905@gmail.com>
2023-08-08 11:27:31 +0900

    add: Crime Database, Dao, TypeConverter 추가

---
+++ app/src/main/java/com/minux/criminalintent/database/CrimeDao.kt
@@ -0,0 +1,15 @@
+package com.minux.criminalintent.database
+
+import androidx.room.Dao
+import androidx.room.Query
+import com.minux.criminalintent.Crime
+import java.util.UUID
+
+@Dao
+interface CrimeDao {
+    @Query("SELECT * FROM crime")
+    fun getCrimes(): List<Crime>
+
+    @Query("SELECT * FROM crime WHERE id=(:id)")
+    fun getCrime(id: UUID): Crime?
+}
