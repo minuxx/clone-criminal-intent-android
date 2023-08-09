5495397368bf144f05fddc8d2ac344402335fe42
minuxx <minux8905@gmail.com>
2023-08-09 11:01:55 +0900

    add: CrimeRepository 추가 및 Database, Dao 설정

--- app/src/main/java/com/minux/criminalintent/CrimeListViewModel.kt
+++ app/src/main/java/com/minux/criminalintent/CrimeListViewModel.kt
@@ -4,14 +4,4 @@ import androidx.lifecycle.ViewModel
 
 class CrimeListViewModel : ViewModel() {
     val crimes = mutableListOf<Crime>()
-
-    init {
-        for (i in 0 until 100) {
-            val crime = Crime()
-            crime.title = "Crime #$i"
-            crime.isSolved = i % 2 == 0
-            crime.requiresPolice = false
-            crimes += crime
-        }
-    }
 }
