d5f4fdad1ded0d7e06cdb444ca524fedb820cd65
minuxx <minux8905@gmail.com>
2023-09-07 16:13:21 +0900

    add: Excutors 활용, CrimeRepository updateCrime, addCrime 로직 추가

--- app/src/main/java/com/minux/criminalintent/CrimeRepository.kt
+++ app/src/main/java/com/minux/criminalintent/CrimeRepository.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.LiveData
 import androidx.room.Room
 import com.minux.criminalintent.database.CrimeDatabase
 import java.util.UUID
+import java.util.concurrent.Executors
 
 private const val DATABASE_NAME = "crime-database"
 
@@ -15,9 +16,22 @@ class CrimeRepository private constructor(context: Context) {
         DATABASE_NAME
     ).build()
     private val crimeDao = database.crimeDao()
+    private val excutor = Executors.newSingleThreadExecutor()
 
     fun getCrimes(): LiveData<List<Crime>> = crimeDao.getCrimes()
     fun getCrime(id: UUID): LiveData<Crime?> = crimeDao.getCrime(id)
+
+    fun updateCrime(crime: Crime) {
+        excutor.execute {
+            crimeDao.updateCrime(crime)
+        }
+    }
+
+    fun addCrime(crime: Crime) {
+        excutor.execute {
+            crimeDao.addCrime(crime)
+        }
+    }
 
     companion object {
         private var INSTANCE: CrimeRepository? = null
