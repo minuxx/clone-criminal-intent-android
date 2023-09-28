037c587f902814a9891e4ba53a981c6366edf5d7
minuxx <minux8905@gmail.com>
2023-09-28 11:21:46 +0900

    add: CrimeListFragment 액션 바, 새로운 범죄 액션 메뉴 추가

--- app/src/main/java/com/minux/criminalintent/CrimeListFragment.kt
+++ app/src/main/java/com/minux/criminalintent/CrimeListFragment.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
@@ -40,6 +42,11 @@ class CrimeListFragment : Fragment() {
         callbacks = context as Callbacks
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -69,6 +76,11 @@ class CrimeListFragment : Fragment() {
     override fun onDetach() {
         super.onDetach()
         callbacks = null
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.fragment_crime_list, menu)
     }
 
     companion object {
